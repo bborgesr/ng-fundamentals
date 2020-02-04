31c441a928156c1227baefc91d33708f6a8579a4
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-02-04 10:14:39 +0000

    adding components and passing data down with @Input

--- src/app/events-app.module.ts
+++ src/app/events-app.module.ts
@@ -2,9 +2,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { EventsAppComponent } from "./events-app.component";
+import { EventsListComponent } from "./events/events-list.component";
+import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 
 @NgModule({
-  declarations: [EventsAppComponent],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
+  ],
   imports: [BrowserModule],
   bootstrap: [EventsAppComponent]
 })
