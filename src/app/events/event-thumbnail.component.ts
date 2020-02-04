b035e293be1607b0957116d36bfd18d553c39529
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-02-04 16:30:20 +0000

    *ngFor, *ngIf, [hidden], ngSwitch, ngClass, ngStyle

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,22 +2,21 @@ import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "event-thumbnail",
-  template: `
-    <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date: {{ event.date }}</div>
-      <div>Time: {{ event.time }}</div>
-      <div>Price: \${{ event.price }}</div>
-      <div>
-        <span>Location: {{ event.location.address }}</span>
-        <span class="pad-left"
-          >{{ event.location.city }}, {{ event.location.country }}</span
-        >
-      </div>
-    </div>
-  `,
+  templateUrl: "./event-thumbnail.component.html",
   styles: [
     `
+      .yellow {
+        color: #907409 !important;
+      }
+      .green {
+        color: #003300 !important;
+      }
+      .bold {
+        font-weight: bold;
+      }
+      .thumbnail {
+        min-height: 210px;
+      }
       .pad-left {
         margin-left: 10px;
       }
@@ -29,4 +28,15 @@ import { Component, Input } from "@angular/core";
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeClass(): any {
+    const isEarlyStart = this.event && this.event.time === "8:00 am";
+    return { green: isEarlyStart, bold: isEarlyStart };
+  }
+
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === "8:00 am")
+      return { "text-decoration": "underline", "font-size": "large" };
+    return {};
+  }
 }
