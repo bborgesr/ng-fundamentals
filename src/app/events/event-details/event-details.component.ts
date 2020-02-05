0504271c820cadeb3775abaec85f6767730d2545
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-02-05 13:57:24 +0000

    styling active links

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
-import { EventService } from "../shared/event.service";
 import { ActivatedRoute } from "@angular/router";
+
+import { EventService } from "../shared/event.service";
+import { ToastrService } from "../../common/toastr.service";
 
 @Component({
   templateUrl: "./event-details.component.html",
@@ -20,11 +22,16 @@ export class EventDetailsComponent {
 
   constructor(
     private eventService: EventService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private toastrService: ToastrService
   ) {}
 
   ngOnInit() {
     const id: number = Number(this.route.snapshot.params["id"]);
     this.event = this.eventService.getEvent(id);
+  }
+
+  handleImageClick() {
+    this.toastrService.success(this.event.name);
   }
 }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 
-import { EventService } from "./shared/event.service";
 import { ToastrService } from "../common/toastr.service";
 import { ActivatedRoute } from "@angular/router";
 
@@ -11,13 +10,11 @@ export class EventsListComponent implements OnInit {
   events: any;
 
   constructor(
-    private eventService: EventService,
     private toastrService: ToastrService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    // this.eventService.getEvents().subscribe(events => (this.events = events));
     this.events = this.route.snapshot.data["events"];
   }
 
