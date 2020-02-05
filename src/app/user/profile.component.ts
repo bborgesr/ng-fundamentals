8735bfe3c56b51016d2a9b35a1f2855abb346993
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-02-05 16:34:41 +0000

    creating a reactive form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,6 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, Inject, forwardRef } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { Router } from "@angular/router";
+
+import { AuthService } from "./auth.service";
 
 @Component({
-  template: "./profile.component.html"
+  templateUrl: "./profile.component.html"
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+
+  constructor(
+    @Inject(forwardRef(() => AuthService)) private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    let firstName = new FormControl(this.authService.currentUser.firstName);
+    let lastName = new FormControl(this.authService.currentUser.lastName);
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
+
+  cancel() {
+    this.router.navigate(["events"]);
+  }
+
+  saveProfile(formValues) {
+    this.authService.updateCurrentUser(
+      formValues.firstName,
+      formValues.lastName
+    );
+    this.router.navigate(["events"]);
+  }
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { userRoutes } from "./user.routes";
 import { ProfileComponent } from "./profile.component";
@@ -9,7 +9,12 @@ import { LoginComponent } from "./login.component";
 
 @NgModule({
   declarations: [ProfileComponent, LoginComponent],
-  imports: [CommonModule, FormsModule, RouterModule.forChild(userRoutes)],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(userRoutes)
+  ],
   providers: []
 })
 export class UserModule {}
