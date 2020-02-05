994c20d00294bcc67bb6d0e5b5361b4c2a5f3b48
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-02-05 11:16:22 +0000

    enable routing

--- src/app/events-app.module.ts
+++ src/app/events-app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 import { EventsAppComponent } from "./events-app.component";
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumbnail.component";
+import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { NavBarComponent } from "./nav/navbar.component";
+
+import { appRoutes } from "./routes";
 
 import { EventService } from "./events/shared/event.service";
 import { ToastrService } from "./common/toastr.service";
@@ -14,9 +18,10 @@ import { ToastrService } from "./common/toastr.service";
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
     NavBarComponent
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
